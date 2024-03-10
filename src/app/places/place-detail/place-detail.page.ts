465862a70b207a0d6b1331e5df5948ce9978b57e
22ftt1512 <22ftt1512@student.pb.edu.bn>
2024-03-10 23:31:07 +0800

    Adding ion modal in the typescript

--- src/app/places/place-detail/place-detail.page.ts
+++ src/app/places/place-detail/place-detail.page.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { cart } from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
+import { IonModal } from '@ionic/angular/standalone';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
   styleUrls: ['./place-detail.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule, IonModal, ],
 })
 export class PlaceDetailPage implements OnInit {
+  @ViewChild(IonModal)
+  modal!: IonModal;
+
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
   constructor() {
     addIcons({
       cart,
